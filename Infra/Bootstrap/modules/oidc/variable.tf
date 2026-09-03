3ab0bfc6a61c93102218de569ac60fe3ab0f9dde
Muscab Ali <muscabali@MacBook-Air-som-tillhor-Muscab.local>
2026-09-03 10:55:00 +0200

    add: define GitHub OIDC module variables

---
+++ Infra/Bootstrap/modules/oidc/variable.tf
@@ -0,0 +1,6 @@
+# Name of the permissions policy attached to the deployment role
+variable "deployment_policy_name" {
+  description = "Name of the GitHub Actions deployment permissions policy"
+  type        = string
+}
+
