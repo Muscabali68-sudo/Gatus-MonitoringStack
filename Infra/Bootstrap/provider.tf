1691bb69a31a0a86290b3db769388c00ec3084cd
Muscab Ali <muscabali@MacBook-Air-som-tillhor-Muscab.local>
2026-09-03 10:28:59 +0200

    Add: AWS provider for Terraform bootstrap

---
+++ Infra/Bootstrap/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.15.6"
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
