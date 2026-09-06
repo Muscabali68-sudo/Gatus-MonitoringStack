cbf54c8f553e7572058d78e4e1cdce4b9ed76970
Muscab Ali <muscabali@MacBook-Air-som-tillhor-Muscab.local>
2026-09-06 19:03:31 +0200

    edit: VPC and subnet outputs

--- Infra/modules/vpc/outputs.tf
+++ Infra/modules/vpc/outputs.tf
@@ -1,15 +1,14 @@
-# Send the VPC ID back to the root module
+# Allow other modules to use the VPC ID
 output "vpc_id" {
   description = "ID of the Gatus VPC"
   value       = aws_vpc.gatus_vpc.id
 }
 
 
-# Send all public subnet IDs back to the root module
+# Allow other modules to use the public subnet IDs
 output "public_subnet_ids" {
   description = "IDs of the public subnets"
 
-  # Keep only subnets where public_ip is true
   value = {
     for key, subnet in aws_subnet.subnets :
     key => subnet.id
@@ -18,14 +17,13 @@ output "public_subnet_ids" {
 }
 
 
-# Send all private subnet IDs back to the root module
+# Allow other modules to use the private subnet IDs
 output "private_subnet_ids" {
   description = "IDs of the private subnets"
 
-  # Keep only subnets where public_ip is false
   value = {
     for key, subnet in aws_subnet.subnets :
     key => subnet.id
     if !var.subnets[key].public_ip
   }
-}
+} 
